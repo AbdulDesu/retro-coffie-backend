65df7453a6b25ed0d5d95094e9b11e08f8d3a5ec
afarelramdani <ahmadfarhanel@gmail.com>
2021-01-21 12:45:16 +0700

    Add Login and Register

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -2,97 +2,96 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const {
-    createAccountModel,
-    getAccountByEmailModel,
-    loginAccountModel
+  createAccountModel,
+  getAccountByEmailModel,
+  loginAccountModel
 } = require('../models/AccountModel')
 
 const { statusRegistration, statusRegistrationFail, statusRegistrationUnique, statusServerError } = require('../helpers/status')
 
 module.exports = {
-    createAccount: async(req, res, _next) => {
-        const { acName, acEmail, acPhone, acPassword } = req.body
-        const salt = bcrypt.genSaltSync(10)
-        const encryptPassword = bcrypt.hashSync(acPassword, salt)
+  createAccount: async (req, res, _next) => {
+    const { acName, acEmail, acPhone, acPassword } = req.body
+    const salt = bcrypt.genSaltSync(10)
+    const encryptPassword = bcrypt.hashSync(acPassword, salt)
 
-        const setData = {
-            ac_name: acName,
-            ac_email: acEmail,
-            ac_phone: acPhone,
-            ac_password: encryptPassword
-        }
-        try {
-            const findData = await getAccountByEmailModel(acEmail)
-            if (!findData.length) {
-                const result = await createAccountModel(setData)
-
-                if (result.affectedRows) {
-                    statusRegistration(res)
-                } else {
-                    statusRegistrationFail(res)
-                }
-            } else {
-                statusRegistrationUnique(res)
-            }
-        } catch (err) {
-            statusServerError(res)
-        }
-    },
-    loginAccount: async(req, res) => {
-        try {
-            const { email, password } = req.body
-
-            const login = await loginAccountModel(email)
-
-            if (login.length >= 1) {
-
-                const cekPsw = bcrypt.compareSync(password, login[0].ac_password)
-                if (cekPsw) {
-                    console.log(login[0])
-                    const {
-                        ac_id,
-                        ac_name,
-                        ac_email,
-                        ac_phone,
-                        ac_level,
-                        ac_status,
-                        cs_id
-                    } = login[0]
-
-                    let peyLoad = {
-                        ac_id,
-                        ac_name,
-                        ac_email,
-                        ac_phone,
-                        ac_level,
-                        ac_status,
-                        cs_id
-                    }
-                    const token = jwt.sign(peyLoad, 'retrocoffee', { expiresIn: '7d' })
-                    peyLoad = {...peyLoad, token }
-                    res.send({
-                        success: true,
-                        message: 'You are login',
-                        data: peyLoad
-                    })
-                } else {
-                    res.status(400).send({
-                        success: false,
-                        message: 'Email or password wrong'
-                    })
-                }
-            } else {
-                res.statu(400).send({
-                    success: false,
-                    message: 'Email or Account not register '
-                })
-            }
-        } catch (error) {
-            console.log(error)
-            res.status(500).send({
-                success: false,
-                message: 'internal server error'
-            })
-        }
+    const setData = {
+      ac_name: acName,
+      ac_email: acEmail,
+      ac_phone: acPhone,
+      ac_password: encryptPassword
     }
+    try {
+      const findData = await getAccountByEmailModel(acEmail)
+      if (!findData.length) {
+        const result = await createAccountModel(setData)
+
+        if (result.affectedRows) {
+          statusRegistration(res)
+        } else {
+          statusRegistrationFail(res)
+        }
+      } else {
+        statusRegistrationUnique(res)
+      }
+    } catch (err) {
+      statusServerError(res)
+    }
+  },
+  loginAccount: async (req, res) => {
+    try {
+      const { email, password } = req.body
+
+      const login = await loginAccountModel(email)
+
+      if (login.length >= 1) {
+        const cekPsw = bcrypt.compareSync(password, login[0].ac_password)
+        if (cekPsw) {
+          console.log(login[0])
+          const {
+            ac_id,
+            ac_name,
+            ac_email,
+            ac_phone,
+            ac_level,
+            ac_status,
+            cs_id
+          } = login[0]
+
+          let peyLoad = {
+            ac_id,
+            ac_name,
+            ac_email,
+            ac_phone,
+            ac_level,
+            ac_status,
+            cs_id
+          }
+          const token = jwt.sign(peyLoad, 'retrocoffee', { expiresIn: '7d' })
+          peyLoad = { ...peyLoad, token }
+          res.send({
+            success: true,
+            message: 'You are login',
+            data: peyLoad
+          })
+        } else {
+          res.status(400).send({
+            success: false,
+            message: 'Email or password wrong'
+          })
+        }
+      } else {
+        res.statu(402).send({
+          success: false,
+          message: 'Email or Account not register '
+        })
+      }
+    } catch (error) {
+      console.log(error)
+      res.status(500).send({
+        success: false,
+        message: 'internal server error'
+      })
+    }
+  }
 }
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,174 +1,173 @@
 const {
-    addProductModel,
-    getAllProductModel,
-    getProductByIDModel,
-    updateProductModel,
-    deleteProductModel
-  } = require('../models/ProductModel')
+  addProductModel,
+  getAllProductModel,
+  getProductByIDModel,
+  updateProductModel,
+  deleteProductModel
+} = require('../models/ProductModel')
 
 const {
-    statusGet,
-    statusCreate,
-    statusCreateFail,
-    statusServerError,
-    statusNotFound,
-    statusUpdate,
-    statusDelete
+  statusGet,
+  statusCreate,
+  statusCreateFail,
+  statusServerError,
+  statusNotFound,
+  statusUpdate,
+  statusDelete
 } = require('../helpers/status')
 
 module.exports = {
-    addProduct: async (req, res, _next) => {
-      req.body.pr_pic_image = req.file === undefined ? '' : req.file.filename
-  
-      const data = {
-        ...req.body,
-        pr_pic_image: req.body.pr_pic_image
+  addProduct: async (req, res, _next) => {
+    req.body.pr_pic_image = req.file === undefined ? '' : req.file.filename
+
+    const data = {
+      ...req.body,
+      pr_pic_image: req.body.pr_pic_image
+    }
+
+    delete data.pr_pic_image
+
+    try {
+      const result = await addProductModel(req.body)
+
+      if (result.affectedRows) {
+        statusCreate(res)
+      } else {
+        statusCreateFail(res)
       }
-  
-      delete data.pr_pic_image
-  
-      try {
-        const result = await addProductModel(req.body)
-  
-        if (result.affectedRows) {
-          statusCreate(res)
-        } else {
-          statusCreateFail(res)
-        }
-      } catch (err) {
-        console.log(err)
-        statusServerError(res)
-      }
-    },
-  
-    getAllProduct: (req, res) => {
+    } catch (err) {
+      console.log(err)
+      statusServerError(res)
+    }
+  },
+
+  getAllProduct: (req, res) => {
     let { search, limit, page } = req.query
     let searchKey = ''
     let searchValue = ''
 
     if (typeof search === 'object') {
-        searchKey = Object.keys(search)[0]
-        searchValue = Object.values(search)[0]
+      searchKey = Object.keys(search)[0]
+      searchValue = Object.values(search)[0]
     } else {
-        searchKey = 'pr_name'
-        searchValue = search || ''
+      searchKey = 'pr_name'
+      searchValue = search || ''
     }
 
     if (!limit) {
-        limit = 25
+      limit = 25
     } else {
-        limit = parseInt(limit)
+      limit = parseInt(limit)
     }
 
     if (!page) {
-        page = 1
+      page = 1
     } else {
-        page = parseInt(page)
+      page = parseInt(page)
     }
 
     const offset = (page - 1) * limit
 
     getAllProductModel(searchKey, searchValue, limit, offset, result => {
-        if (result.length) {
-            statusGet(res, result)
-        } else {
-            statusNotFound(res)
-         }
-        })
-    },
-  
-    getProductById: async (req, res, _next) => {
-      const { pr_id } = req.params
-  
-      try {
-        const result = await getProductByIDModel(pr_id)
-        if (result.length) {
-          statusGet(res, result)
-        } else {
-          statusNotFound(res)
-        }
-      } catch (error) {
-        console.error(error)
-        statusServerError(res)
+      if (result.length) {
+        statusGet(res, result)
+      } else {
+        statusNotFound(res)
       }
-    },
-  
-    updateProduct: async (req, res, _next) => {
-      const { pr_id } = req.params
-  
-      try {
-        const caughtData = await getProductByIDModel(pr_id)
-  
-        if (caughtData.length) {
-          const result = await updateProductModel(pr_id, req.body)
-  
-          if (result.affectedRows) {
-            statusUpdate(res, result)
-          } else {
-            statusNotFound(res)
-          }
+    })
+  },
+
+  getProductById: async (req, res, _next) => {
+    const { pr_id } = req.params
+
+    try {
+      const result = await getProductByIDModel(pr_id)
+      if (result.length) {
+        statusGet(res, result)
+      } else {
+        statusNotFound(res)
+      }
+    } catch (error) {
+      console.error(error)
+      statusServerError(res)
+    }
+  },
+
+  updateProduct: async (req, res, _next) => {
+    const { pr_id } = req.params
+
+    try {
+      const caughtData = await getProductByIDModel(pr_id)
+
+      if (caughtData.length) {
+        const result = await updateProductModel(pr_id, req.body)
+
+        if (result.affectedRows) {
+          statusUpdate(res, result)
         } else {
           statusNotFound(res)
         }
-      } catch (error) {
-        console.error(error)
-        statusServerError(res)
+      } else {
+        statusNotFound(res)
       }
-    },
-  
-    updateProductWithImage: async (req, res, _next) => {
-      const { pr_id } = req.params
-  
-      req.body.pr_pic_image = req.file === undefined ? '' : req.file.filename
-  
-      const data = {
-        ...req.body,
-        pr_pic_image: req.body.pr_pic_image
-      }
-  
-      delete data.pr_pic_image
-  
-      try {
-        const caughtData = await getProductByIDModel(pr_id)
-  
-        if (caughtData.length) {
-          const result = await updateProductModel(pr_id, req.body)
-  
-          if (result.affectedRows) {
-            statusUpdate(res, result)
-          } else {
-            statusNotFound(res)
-          }
+    } catch (error) {
+      console.error(error)
+      statusServerError(res)
+    }
+  },
+
+  updateProductWithImage: async (req, res, _next) => {
+    const { pr_id } = req.params
+
+    req.body.pr_pic_image = req.file === undefined ? '' : req.file.filename
+
+    const data = {
+      ...req.body,
+      pr_pic_image: req.body.pr_pic_image
+    }
+
+    delete data.pr_pic_image
+
+    try {
+      const caughtData = await getProductByIDModel(pr_id)
+
+      if (caughtData.length) {
+        const result = await updateProductModel(pr_id, req.body)
+
+        if (result.affectedRows) {
+          statusUpdate(res, result)
         } else {
           statusNotFound(res)
         }
-      } catch (error) {
-        console.error(error)
-        statusServerError(res)
+      } else {
+        statusNotFound(res)
       }
-    },
-  
-    deleteProduct: async (req, res, _next) => {
-      const { pr_id } = req.params
-  
-      try {
-        const caughtData = await getProductByIDModel(pr_id)
-  
-        if (caughtData.length) {
-          const result = await deleteProductModel(pr_id)
-  
-          if (result.affectedRows) {
-            statusDelete(res, result)
-          } else {
-            statusNotFound(res)
-          }
+    } catch (error) {
+      console.error(error)
+      statusServerError(res)
+    }
+  },
+
+  deleteProduct: async (req, res, _next) => {
+    const { pr_id } = req.params
+
+    try {
+      const caughtData = await getProductByIDModel(pr_id)
+
+      if (caughtData.length) {
+        const result = await deleteProductModel(pr_id)
+
+        if (result.affectedRows) {
+          statusDelete(res, result)
         } else {
           statusNotFound(res)
         }
-      } catch (error) {
-        console.error(error)
-        statusServerError(res)
+      } else {
+        statusNotFound(res)
       }
+    } catch (error) {
+      console.error(error)
+      statusServerError(res)
     }
   }
-  
+}
